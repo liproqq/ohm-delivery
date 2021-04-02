7a78aa2a1eebc38a9146fe7178e4c8d0687bf48f
Manuel Kessel <liproqq@gmail.com>
2021-04-02 15:30:41 +0200

    Basic UI

--- web/app/main.js
+++ web/app/main.js
@@ -1,12 +1,28 @@
 angular
     .module("ohm-delivery", [])
-    .controller("tracking", function($scope, $http) {
-        $scope.sendData = function() {
+    .controller("tracking", function ($scope, $http) {
+        $scope.sendData = function () {
             $http.get(`/ohms/${this.trackingId}`)
-            .then((error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            }, (result) => {
-                this.errorMessage = '';
-            });
+                .then((result) => {
+                    if (result.data.status == "IN_DELIVERY") {
+                        this.trackingData = result.data;
+                        console.log(result.data)
+                    } else {
+                        this.errorMessage = "Your order is not yet in delivery or was finalized by the customer"
+                    }
+                }, (error) => {
+                    this.errorMessage = 'Something went wrong.';
+                });
         };
+        $scope.confirmDelivery = function (status, comment) {
+            if (status) {
+                $http.post(`/ohms/finalize/${this.trackingId}`, { status: status, comment: comment })
+                    .then((result) => {
+                        this.trackingData = '';
+                        this.confirmationMessage = 'Your feedback was processed. Thank you!'
+                    }, (error) => {
+                        this.errorMessage = 'Something went wrong.';
+                    });
+            }
+        }
     });
