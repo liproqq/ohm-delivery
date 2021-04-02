782d88929817989b4fc3b210d8bcd3628de54fbc
Manuel Kessel <liproqq@gmail.com>
2021-04-02 15:30:54 +0200

    fixed and added tests

--- server/__test__/utils.test.js
+++ server/__test__/utils.test.js
@@ -1,12 +1,37 @@
 const utils = require('../src/utils');
 
-describe('db return ohm', () => {
-    test('returns Ohm object', () => {
-        expect(utils.getOhmById('1').toBeDefined());
+describe('getOhmById:', () => {
+    test('returns Ohm object', async () => {
+        const ohm = await utils.getOhmById('1');
+        expect(ohm).toBeDefined();
     });
 
-    test('has a valid history', () => {
-        const ohm = utils.getOhmById('1');
+    test('return undefined for invalid queries', async () => {
+        const ohm = await utils.getOhmById('But it works on my machine');
+        expect(ohm).toBeUndefined();
+    });
+
+    test('has a valid history', async () => {
+        const ohm = await utils.getOhmById('1');
+        const statuses = [ 'CREATED', 'PREPARING', 'READY', 'IN_DELIVERY','DELIVERED', 'REFUSED']
+        const isValidStatus = statuses.includes(ohm.history[0].state)
+        expect(isValidStatus).toBe(true);
+    });
+})
+describe('getOhmByTrackingId:', () => {
+    const trackingId = '1e62adfe'
+    test('returns Ohm object', async () => {
+        const ohm = await utils.getOhmByTrackingId(trackingId);
+        expect(ohm).toBeDefined();
+    });
+
+    test('return undefined for invalid queries', async () => {
+        const ohm = await utils.getOhmByTrackingId('it should work now');
+        expect(ohm).toBeUndefined();
+    });
+
+    test('has a valid history', async () => {
+        const ohm = await utils.getOhmByTrackingId(trackingId);
         const statuses = [ 'CREATED', 'PREPARING', 'READY', 'IN_DELIVERY','DELIVERED', 'REFUSED']
         const isValidStatus = statuses.includes(ohm.history[0].state)
         expect(isValidStatus).toBe(true);
